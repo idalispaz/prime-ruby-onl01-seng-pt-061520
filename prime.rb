4a99b22b5b282680549f925a87dc49afa1600ca4
Idalis Paz <idalispaz@gmail.com>
2020-06-27 03:38:48 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,12 @@
+def prime?(number)
+  divisors = *(2..number - 1)
+  remainders = []
+  divisors.each do |divisor|
+    remainder = number % divisor
+    remainders << remainder
+end
+  if number > 1 then remainders. all? {|remainder| remainder != 0}
+  else
+    false
+  end
+  end
